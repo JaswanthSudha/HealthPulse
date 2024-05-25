9cf2506d76819b6938a4b72899506c17bcce88e6
Jaswanth Sudha <119607624+JaswanthSudha@users.noreply.github.com>
2024-05-25 13:14:31 +0530

    seconday

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const path = require("path")
 const cors = require('cors')
+const reportModel = require("./models/report")
 const { createReport } = require("./controllers/report")
 app.use(cors())
 app.use(express.json())
@@ -22,16 +23,33 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 app.post("/upload", upload.single("profile"), async (req, res) => {
     try {
-        console.log(req.file);
         if (req.file) {
-            await createReport(req, res)
+            console.log(req.file);
+            const { longtitude, latitude } = req.body
+            const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longtitude}`;
+            const address = await fetch(url, {
+                headers: {
+                    'User-Agent': 'JaswanthSudha (jaswanthsudha2021@gmail.com)'
+                }
+            })
+            if (!address) {
+                throw new Error("Address Not Found")
+            }
+            const addressjson = await address.json()
+            console.log(addressjson);
+            const report = await reportModel.create({ value: "Not Malnutrioned", address: addressjson.address.county, user_id: 1 })
+            if (!report) {
+                throw new Error("Report Not Created")
+            }
+            res.status(200).json(report);
         }
         else {
-            throw new Error("File Not Uploaded Successfully")
+            throw new Error("File Not Uploaded")
         }
     }
     catch (error) {
-        res.json(error)
+        console.log(error)
+        res.status(500).json(error)
     }
 })
 mongoose.connect(process.env.STRING)
